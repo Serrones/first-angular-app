28a3231ff4a617f8ed1248100d8c488f87cb4408
Serrones <fabioserrones@gmail.com>
2018-06-02 17:38:41 -0300

    Data Binding- Creating Username

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,9 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
     allowNewServer = false;
     serverCreationStatus = 'No server was created!';
-    serverName = 'Input your New Server Here!!';
+    serverName = '';
+    userNameStatus = '';
+    userName = '';
 
   constructor() {
       setTimeout(() => {
@@ -23,9 +25,12 @@ export class ServersComponent implements OnInit {
       }
 
   onCreateServer() {
-      this.serverCreationStatus = 'Server was created!';
+      this.serverCreationStatus = 'Server was created! The name is ' + this.serverName;
   }
   onUpdateServerName(event: Event) {
       this.serverName= (<HTMLInputElement>event.target).value;
   }
+  onCreateUsername() {
+      this.userNameStatus = 'Username created! Welcome ' + this.userName;
+     }
 }
