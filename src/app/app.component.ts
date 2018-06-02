39bf3ffbbdfc50dd8c2623acac8426c1bbf9210c
Serrones <fabioserrones@gmail.com>
2018-06-02 11:48:19 -0300

    Working with Component Styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+    h1 {
+        color: violet;
+    }
+
+
+  `]
 })
 export class AppComponent {
   title = 'app';
