e92da1d8afcbaa9bac70947adb1e7e0de8cef1f8
Serrones <fabioserrones@gmail.com>
2018-06-02 15:41:13 -0300

    Data Binding-String Interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
+    serverID: number = 100;
+    serverStatus: string = 'offline';
 
+    getServerStatus () {
+        return this.serverStatus;
+    }
 }
